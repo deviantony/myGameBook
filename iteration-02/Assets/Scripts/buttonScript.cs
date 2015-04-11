475a0eb93171d09ed6e8b7d7ce7be2b618404370
deviantony <lapenna.anthony@gmail.com>
2015-04-11 19:27:12 +0200

    Using any buttons is now updating the text displayed.

--- iteration-02/Assets/Scripts/buttonScript.cs
+++ iteration-02/Assets/Scripts/buttonScript.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class buttonScript : MonoBehaviour {
 
+	public Text text;
+
 	public void buttonPushed() {
 		Debug.Log("Pushed !");
+		text.text = "New text is pushed";
 	}
 
 	public void buttonTriggered() {
 		Debug.Log("Triggered !");
+		text.text = "New text is triggered";
 	}
 }
