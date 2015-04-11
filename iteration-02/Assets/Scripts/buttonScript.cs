e9a625429ea0505f3915dc23aa72af73c44d1094
deviantony <lapenna.anthony@gmail.com>
2015-04-11 19:15:37 +0200

    Updated project structure

---
+++ iteration-02/Assets/Scripts/buttonScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonScript : MonoBehaviour {
+
+	public void buttonPushed() {
+		Debug.Log("Pushed !");
+	}
+
+	public void buttonTriggered() {
+		Debug.Log("Triggered !");
+	}
+}
